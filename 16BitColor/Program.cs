2baa2846dfc0e39a2c63d3ccd5404760157add5a
Deukhoofd <deukhoofd@gmail.com>
2016-08-17 02:58:07 +0200

    Initial commit

---
+++ 16BitColor/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _16BitColor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Red:");
+            var redstr = Console.ReadLine();
+            Console.Write("Green:");
+            var greenstr = Console.ReadLine();
+            Console.Write("Blue:");
+            var bluestr = Console.ReadLine();
+
+            var red = short.Parse(redstr);
+            var green = short.Parse(greenstr);
+            var blue = short.Parse(bluestr);
+
+            short val = GetShort(red, green, blue);
+
+            Console.WriteLine(val.ToString("X"));
+            Console.ReadLine();
+        }
+        public static short GetShort(short red, short green, short blue)
+        {
+            red >>= 3;
+            green >>= 2;
+            blue >>= 3;
+            return (short)((red << 11) | (green << 5) | blue);
+        }
+    }
+}
